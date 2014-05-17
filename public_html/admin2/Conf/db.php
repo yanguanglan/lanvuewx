77f5c25c513d87ab1713c45a5dd70e3a48f3c5a7
yuemingist <yueming1985@gmail.com>
2014-05-17 18:34:04 +0800

    Update db.php
    config

--- public_html/admin2/Conf/db.php
+++ public_html/admin2/Conf/db.php
@@ -1,11 +1,11 @@
 <?php
 return array (
   'DB_TYPE' => 'mysql',
-  'DB_HOST' => 'localhost',
+  'DB_HOST' => 'qiwur002.mysql.rds.aliyuncs.com',
   'DB_PORT' => '3306',
   'DB_NAME' => 'weixin8',
-  'DB_USER' => 'root',
-  'DB_PWD' => 'weixin-app.taobao.com',
-  'DB_PREFIX' => 'ai9me_',
+  'DB_USER' => 'dbd1p6q1r0t938v3',
+  'DB_PWD' => '432916mysql',
+  'DB_PREFIX' => '',
 );
 ?>
